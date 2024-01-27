2eb926e835a57d5cf1168ae29c8d138076861728
Esra Nur Yiğit <147344292+esraayigit@users.noreply.github.com>
2024-01-27 14:26:45 +0300

    Add files via upload

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  const date = 'date';
+  const nameSurname = "Esra Nur Yiğit"
+  const students =43000; 
+  const dogruMu = false;
+  const url = "https://picsum.photos/250/250"
+  return (
+   /* <div>
+
+<h1>İsim soyisim: {nameSurname}</h1>
+<p></p>
+<p></p>
+<input type="text" />
+{dogruMu ? <p>Öğrenci sayısı</p> : <p>Kurs sayısı</p>  }
+<input type={date} />
+<img src={url} alt="" />
+    </div>
+    */
+
+    <div style={{
+      width: '250px',
+      height: '250px',
+      backgroundColor: 'blue',
+      fontSize: '30px',
+     
+
+    }}>
+
+
+    </div>
+  ) 
+}
+
+
+export default App;
